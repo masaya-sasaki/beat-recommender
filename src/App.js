5275a837eb96af9df10feb1ee5b882cc72e6330f
Masaya <masaya.sasaki.99@gmail.com>
2022-03-23 21:44:29 -0400

    Setup the main components with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
 import './App.css';
+import Recommendation from './components/Recommendation/Recommendation';
+import SearchBar from './components/SearchBar/SearchBar';
+import SearchResults from './components/SearchResults/SearchResults';
+import SongList from './components/SongList/SongList';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+        <div className='container'>
+          <h1>beat-recommender</h1>
+            <SearchBar>
+              This is the searchbar
+            </SearchBar>
+          <div className='main'>
+            <SearchResults>
+              This is the search results
+            </SearchResults>
+            <SongList>
+              This is the song list 
+            </SongList>
+          </div>
+          <Recommendation>
+            This is the result recommendation 
+          </Recommendation>
+        </div>
   );
 }
 
---
+++ src/components/Recommendation/Recommendation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recommendation = () => {
+  return (
+    <div>Try and find a Cmajor beat with a tempo around 80 BPM. </div>
+  )
+}
+
+export default Recommendation
